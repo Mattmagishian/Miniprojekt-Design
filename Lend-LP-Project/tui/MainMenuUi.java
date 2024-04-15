cc350b44deeaf5bd392f4c66897c8dc4279f66b7
VietTitans <mattnhudinh1@gmail.com>
2024-04-15 10:06:57 +0200

    Implemented print outputs for mainMenu()

---
+++ Lend-LP-Project/tui/MainMenuUi.java
@@ -0,0 +1,14 @@
+package tui;
+
+public class MainMenuUi {
+    
+    public void mainMenu(){
+        System.out.println("****** Main menu ******");
+        System.out.println("(1) Friends menu");
+        System.out.println("(2) LP menu");
+        System.out.println("(3) Loan menu");
+        
+        System.out.println("(9) Quit the program");
+    }
+
+}
