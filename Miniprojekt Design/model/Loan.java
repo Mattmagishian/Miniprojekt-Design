bdb6797449adaabcabd71f155776518aaf3bd1c7
Mims <142897245+Mimsie98@users.noreply.github.com>
2024-04-16 09:42:43 +0200

    Loan class

--- Miniprojekt Design/model/Loan.java
+++ Miniprojekt Design/model/Loan.java
@@ -9,27 +9,57 @@ package model;
  */
 public class Loan
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    //fields
+    private int loanNo;
+    private String borrowDate;
+    private int period;
+    private boolean status;
 
-    /**
-     * Constructor for objects of class Loan
-     */
-    public Loan()
-    {
-        // initialise instance variables
-        x = 0;
+    //constructor
+    public Loan(int loanNo, String borrowDate, int period, boolean status) {
+        this.loanNo = loanNo;
+        this.borrowDate = borrowDate;
+        this.period = period;
+        this.status = status;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    //getters - accessors
+    public int getLoanNo() {
+        return loanNo;
+    }
+
+    public String getBorrowDate() {
+        return borrowDate;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    //setters - mutators
+    public void setLoanNo(int loanNo) {
+        this.loanNo = loanNo;
+    }
+
+    public void setBorrowDate(String borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    //empty stub
+    public boolean addCopyToLoan(Copy copy) {
+        //TODO
+        return false;
     }
 }
