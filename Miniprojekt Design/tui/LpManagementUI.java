0052273206d28918ed112aa05c1181b3f4621713
Mims <142897245+Mimsie98@users.noreply.github.com>
2024-04-16 09:21:23 +0200

    BlueJ

---
+++ Miniprojekt Design/tui/LpManagementUI.java
@@ -0,0 +1,29 @@
+package tui;
+import controller.*;
+import model.*;
+
+
+/**
+ * Write a description of class LpManagementUI here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LpManagementUI
+{
+    private LendLpController lendLpController;
+    private FriendController friendController;
+    private LpController lpcontroller;
+    
+    
+
+    /**
+     * Constructor for objects of class LpManagementUI
+     */
+    public LpManagementUI()
+    {
+        
+    }
+
+    
+}
