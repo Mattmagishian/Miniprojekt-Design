759825880de3410c4468a4fae39607cc589e7cf5
Mims <142897245+Mimsie98@users.noreply.github.com>
2024-04-16 09:33:00 +0200

    Lp class

--- Miniprojekt Design/model/Lp.java
+++ Miniprojekt Design/model/Lp.java
@@ -10,26 +10,54 @@ package model;
 public class Lp
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private String barcode;
+    private String title;
+    private String artist;
+    private String publicationDate;
 
     /**
      * Constructor for objects of class Lp
      */
-    public Lp()
+    public Lp(String barcode, String title, String artist, String publicationDate)
     {
-        // initialise instance variables
-        x = 0;
+        this.barcode = barcode;
+        this.title = title;
+        this.artist = artist;
+        this.publicationDate = publicationDate;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public String getBarcode()
     {
-        // put your code here
-        return x + y;
+        return barcode;
     }
+    
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getArtist() {
+        return artist;
+    }
+    
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+    
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+    
+    
 }
