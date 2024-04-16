1eabda838f7f903b483fbc9c1c67058c7f9148f0
sarsum33 <sar.sum33@gmail.com>
2024-04-16 13:53:46 +0200

    lendlp update

--- Miniprojekt Design/controller/LendLpController.java
+++ Miniprojekt Design/controller/LendLpController.java
@@ -11,13 +11,14 @@ public class LendLpController
     }
     
     public void createLoan(int loanNo, String borrowDate, int period, boolean status, Friend loaner) {
-        //TODO implement
         Loan newLoan = new Loan(loanNo, borrowDate, period, status, loaner);
     }
     
     public Friend findFriendByPhoneNo(String phoneNo) {
         //TODO
-        return null;
+        FriendController friendController = new FriendController();
+        Friend friend = friendController.findFriendByPhoneNo(phoneNo);
+        return friend;
     }
     
     public Copy findCopyBySerialNo(int serialNo) {
@@ -27,6 +28,7 @@ public class LendLpController
     
     public boolean addCopyToLoan(Copy copy) {
         //TODO
+        
         return false;
     }
     
