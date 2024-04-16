502e360e0fe1b12f8ed91cc3447db0305973eeea
VietTitans <mattnhudinh1@gmail.com>
2024-04-16 11:02:37 +0200

    Updated LpManagementUI

--- Miniprojekt Design/tui/LpManagementUI.java
+++ Miniprojekt Design/tui/LpManagementUI.java
@@ -2,28 +2,15 @@ package tui;
 import controller.*;
 import model.*;
 
-
-/**
- * Write a description of class LpManagementUI here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class LpManagementUI
 {
     private LendLpController lendLpController;
     private FriendController friendController;
     private LpController lpcontroller;
     
-    
-
-    /**
-     * Constructor for objects of class LpManagementUI
-     */
     public LpManagementUI()
     {
         
     }
-
     
 }
