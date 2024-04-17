72bf844d1f3ef972055e7c7da0631aafc555724b
VietTitans <mattnhudinh1@gmail.com>
2024-04-17 12:46:20 +0200

    Updated UI

--- Miniprojekt Design/tui/MainMenu.java
+++ Miniprojekt Design/tui/MainMenu.java
@@ -1,6 +1,7 @@
 package tui;
 import controller.*;
 import model.*;
+import java.util.Scanner;
 
 public class MainMenu
 {
@@ -8,11 +9,39 @@ public class MainMenu
     private FriendController friendController;
     private LpController lpcontroller;
     
-    
+    Scanner scanner = new Scanner(System.in);
     public MainMenu()
     {
-        System.out.print("****");
     }
+    
+    public void mainMenu(){
+        boolean running = true; 
+        while (running){
+            int choice = getUserInput();
+        }
+    }
+    
+    public void printMainMenu(){
+        System.out.println("*******************");
+        System.out.println("*** Main Menu ***");
+        System.out.println("(1) Lend LP");
+        System.out.println("(2) Manage LPs");
+        System.out.println("(3) Return LP");
+        System.out.println("(4) Manage friends");
+
+        System.out.println("(0) Exit");
+    }
+    
+    public int getUserInput(){
+        while (!scanner.hasNextInt()){
+            System.out.println("Enter valid input");
+            scanner.nextLine();
+        }
+        int userInput = scanner.nextInt();
+        scanner.nextLine();
+        return userInput;
+    }
+    
     
     public Friend findFriendByPhoneNo(String phoneNo){
         //TODO
